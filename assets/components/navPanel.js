8d19e9a08f05565ef7fe793262e8a4d9aaf9678b
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2026-03-28 22:37:04 -0400

    cleaned up header and nav panel

--- assets/components/navPanel.js
+++ assets/components/navPanel.js
@@ -1,3 +1,5 @@
+// https://www.w3schools.com/howto/howto_js_collapse_sidepanel.asp
+
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
   document.getElementById("navPanel").style.width = "250px";
